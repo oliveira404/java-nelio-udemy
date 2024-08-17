c0570fe71512ea7a2298eda98d8442fbc35e1d67
oliveira404 <lrmcpe22@gmail.com>
2024-08-17 14:32:10 -0300

    tresset

---
+++ mod19-generics-review/src/main/java/_12_exercicio_resolvido_logs_site/Program.java
@@ -0,0 +1,23 @@
+package _12_exercicio_resolvido_logs_site;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+//        TimeZone tz = TimeZone.getTimeZone("UTC");
+//        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'"); // Quoted "Z" to indicate UTC, no timezone offset
+//        df.setTimeZone(tz);
+//        String nowAsISO = df.format(new Date());
+//
+//        System.out.println(nowAsISO);
+
+        
+
+    }
+}
