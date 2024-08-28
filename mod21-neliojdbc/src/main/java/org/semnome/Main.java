00ed3e23dbad6cb2f46263af1121abdb4d309791
oliveira404 <lrmcpe22@gmail.com>
2024-08-28 18:25:34 -0300

    fazendo transações com commit e rollback usando JDBC.

--- mod21-neliojdbc/src/main/java/org/semnome/Main.java
+++ mod21-neliojdbc/src/main/java/org/semnome/Main.java
@@ -1,9 +1,12 @@
 package org.semnome;
 
 import org.semnome.db.DB;
+import org.semnome.db.DbException;
 import org.semnome.db.DbIntegrityException;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -11,19 +14,39 @@ public class Main {
     public static void main(String[] args) {
 
         Connection conn = null;
-        PreparedStatement st = null;
+        Statement st = null;
 
         try {
-            // para todos do departamento de codigo 2 vai setar um aumento de 100 reais a mais.
             conn = DB.getConnection();
-            st = conn.prepareStatement("DELETE FROM department WHERE id = ?");
-            st.setInt(1, 2); //o primeiro é o ? o segundo é a linha da coluna.
 
-            int rowsAffected = st.executeUpdate();
+            // não é para confirmas as alterações automaticamente
+            conn.setAutoCommit(false);
 
-            System.out.println("Done! Rows Affected: " + rowsAffected);
+            st = conn.createStatement();
+
+            int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+
+            int x = 1;
+            if (x < 2) {
+                throw new SQLException("Fake error!");
+            }
+
+            int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+
+            // agora ttodo bloco acima tá protegido
+            conn.commit();
+
+            System.out.println("rows1 " + rows1);
+            System.out.println("rows2 " + rows2);
+
         } catch (SQLException e) {
-            throw new DbIntegrityException(e.getMessage());
+            try {
+                // para voltar ao estado inicial do banco
+                conn.rollback();
+                throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
+            } catch (SQLException e2) {
+                throw new DbException("Error Trying to rollback! Caused by: " + e2.getMessage());
+            }
         } finally {
             DB.closeStatement(st);
             DB.closeConnection();
