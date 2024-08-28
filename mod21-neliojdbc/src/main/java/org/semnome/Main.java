8cdc4c08b4f8731077087ba07e726e395ae175f3
oliveira404 <lrmcpe22@gmail.com>
2024-08-28 15:06:50 -0300

    puxando dados do banco para o console

--- mod21-neliojdbc/src/main/java/org/semnome/Main.java
+++ mod21-neliojdbc/src/main/java/org/semnome/Main.java
@@ -3,11 +3,38 @@ package org.semnome;
 import org.semnome.db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
 
-        Connection connection = DB.getConnection();
-        DB.closeConnection();
+        //Connection connection = DB.getConnection();
+        //DB.closeConnection();
+
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("SELECT * FROM department");
+
+            // enquanto existir uma proxima linha
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + ", " + rs.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            // fizemos assim para nao ficarmos criando vários try catch aqui.
+            // estamos fechando pois a jvm não conrola essas conexões, então para evitar consuma fechamos elas
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
     }
 }
--- mod21-neliojdbc/src/main/java/org/semnome/db/DB.java
+++ mod21-neliojdbc/src/main/java/org/semnome/db/DB.java
@@ -3,9 +3,7 @@ package org.semnome.db;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -21,9 +19,9 @@ public class DB {
                 String username = "root";
                 String password = "0122";
                 connection = DriverManager.getConnection(url, username, password);
-                System.out.println("Connection Successfully!");
+                System.out.println("\u001B[92mConnection Successfully!\u001B[0m\n");
             } catch (SQLException | ClassNotFoundException e) {
-                System.out.println("Connection cannot be conclude!");
+                System.out.println("\u001B[91mConnection cannot be conclude!\u001B[0m\n");
                 throw new DbException(e.getMessage());
             }
         }
@@ -34,7 +32,27 @@ public class DB {
         if (connection != null) {
             try {
                 connection.close();
-                System.out.println("Connection Closed!");
+                System.out.println("\n\u001B[93mConnection Closed!\u001B[0m");
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeStatement(Statement statement) {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet resultSet) {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
             } catch (SQLException e) {
                 throw new DbException(e.getMessage());
             }
