ff343ff1cab88a9e031114447a0e6e447fd3a47c
oliveira404 <lrmcpe22@gmail.com>
2024-08-28 20:29:39 -0300

    Projeto completo DAO - criação do seller

--- mod21-dao-projeto-completo/src/main/java/com/projetocompleto/application/Program.java
+++ mod21-dao-projeto-completo/src/main/java/com/projetocompleto/application/Program.java
@@ -1,12 +1,16 @@
 package com.projetocompleto.application;
 
 import com.projetocompleto.model.entities.Department;
+import com.projetocompleto.model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department department = new Department(1, "books");
-        System.out.println(department);
+        Seller seller = new Seller(21, "bobo", "bobo@gmail", new Date(), 300.0, department);
+        System.out.println(seller);
     }
 }
