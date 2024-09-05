5e8af6de59c37df2b834e8ea24b74604b2793abf
oliveira404 <lrmcpe22@gmail.com>
2024-09-04 23:39:49 -0300

    Projeto Spring Completo - H2 database, Setting Profile Test

--- mod23-spring/course/src/main/java/com/meusestudos/course/entities/User.java
+++ mod23-spring/course/src/main/java/com/meusestudos/course/entities/User.java
@@ -1,5 +1,10 @@
 package com.meusestudos.course.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -10,10 +15,15 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
+
     private static final long serialVersionUID = 1L;
 
+    @Id
     @EqualsAndHashCode.Exclude
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
